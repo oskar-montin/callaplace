b247e4d72b1b51f2a8c3a2b9a9e91558f0eb585a
Oskar Montin <oskar.monton@netclean.com>
2016-05-13 15:49:24 +0200

    hello world init

---
+++ call-a-place/app/src/androidTest/java/com/callaplace/call_a_place/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.callaplace.call_a_place;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ call-a-place/app/src/main/java/com/callaplace/call_a_place/MainActivity.java
@@ -0,0 +1,13 @@
+package com.callaplace.call_a_place;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
