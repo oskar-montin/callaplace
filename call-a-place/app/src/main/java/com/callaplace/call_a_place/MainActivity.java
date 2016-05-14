5e38322ce80f9c3e10dfcc737274562ca608356c
Marcus Hultman <marcus.hultman1@gmail.com>
2016-05-14 16:24:40 +0200

    Updated API key, UI java stuff

--- call-a-place/app/src/main/java/com/callaplace/call_a_place/MainActivity.java
+++ call-a-place/app/src/main/java/com/callaplace/call_a_place/MainActivity.java
@@ -1,13 +1,113 @@
 package com.callaplace.call_a_place;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
+public class MainActivity extends AppCompatActivity implements
+        TabLayout.OnTabSelectedListener,
+        OnMapReadyCallback, GoogleMap.OnMapClickListener,
+        GoogleMap.OnMarkerClickListener,
+        LocationListener {
+
+    private GoogleMap mMap;
+
+    private FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+
+        setupBottomSheet();
+        setupTabIconTints();
+
+        // Initialize map
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
     }
+
+    private void setupTabIconTints() {
+        final TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        for (int i = 0; i < tabs.getTabCount(); i++){
+            tabs.getTabAt(i).getIcon().setTint(
+                    getResources().getColor(R.color.colorWhite));
+        }
+    }
+
+    private void setupBottomSheet() {
+        LinearLayout bottomSheet = (LinearLayout) findViewById(R.id.bottom_sheet);
+        final BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
+
+        // Initial behavior
+        behavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        // TODO: permissions
+        mMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+
 }
